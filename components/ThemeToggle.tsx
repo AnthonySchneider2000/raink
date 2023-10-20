dd1fed0fd3bb10206c00a5129c25740d91912b94
Anthony Schneider <tonyschneider3@gmail.com>
2023-10-20 18:54:04 -0500

    theme based on props instead of context

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -1,32 +1,25 @@
 "use client";
 
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {
   MdBrightness3 as DarkMode,
   MdBrightnessHigh as LightMode,
 } from "react-icons/md";
-import { ThemeProvider, useThemeContext } from "@/lib/ThemeContext";
 
 interface ThemeToggleProps {
   currentTheme?: string;
 }
 
 export default function ThemeToggle({ currentTheme }: ThemeToggleProps) {
-  const { isDarkMode, toggleDarkMode } = useThemeContext();
-
-  const handleDarkMode = () => {
+  const [isDarkMode, setIsDarkMode] = useState(currentTheme === "dark");
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
     document.documentElement.classList.toggle("dark");
-    console.log(document);
-    toggleDarkMode();
-    let darkModeString: string;
   };
-
   return (
-    <ThemeProvider>
-      <div onClick={handleDarkMode} className="absolute top-0 right-0 m-4">
+      <div onClick={toggleDarkMode} className="absolute top-0 right-0 m-4">
         {isDarkMode ? <LightMode size="2rem" /> : <DarkMode size="2rem" />}
       </div>
-    </ThemeProvider>
   );
 }
