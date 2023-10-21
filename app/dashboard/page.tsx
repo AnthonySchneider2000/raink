7625399eb5d51e307b68cef9d8e25eaf3ec419cb
Anthony Schneider <tonyschneider3@gmail.com>
2023-10-21 02:54:03 -0500

    dashboard: added skeletons for placeholder

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Dashboard() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-24 px-16">
+    <main className="flex min-h-screen flex-col items-center justify-start gap-[5vh] py-24 px-16">
       <h1>Dashboard</h1>
+      <Skeleton className="w-full h-16" />
+      <Skeleton className="w-full h-32" />
+      <div className="grid grid-cols-2 gap-8 w-full">
+        <Skeleton className="w-full h-32" />
+        <Skeleton className="w-full h-32" />
+      </div>
+
+      <div className="grid grid-cols-4 gap-8 w-full">
+        <Skeleton className="w-full h-16" />
+        <Skeleton className="w-full h-16" />
+        <Skeleton className="w-full h-16" />
+        <Skeleton className="w-full h-16" />
+        <Skeleton className="w-full h-16" />
+        <Skeleton className="w-full h-16" />
+        <Skeleton className="w-full h-16" />
+        <Skeleton className="w-full h-16" />
+        <Skeleton className="w-full h-32" />
+        <Skeleton className="w-full h-32" />
+        <Skeleton className="w-full h-32" />
+        <Skeleton className="w-full h-32" />
+
+      </div>
     </main>
-  )
+  );
 }
