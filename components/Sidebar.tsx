1e949738add04e56d002dc7369097ec76db87105
Anthony Schneider <tonyschneider3@gmail.com>
2023-10-20 20:00:34 -0500

    ui: replaced sidebar content with nav links

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -24,30 +24,18 @@ export default function Sidebar() {
       </SheetTrigger>
       <SheetContent side="left">
         <SheetHeader>
-          <SheetTitle>Edit profile</SheetTitle>
-          <SheetDescription>
-            Make changes to your profile here. Click save when you're done.
-          </SheetDescription>
+          <SheetTitle>Links</SheetTitle>
         </SheetHeader>
         <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
-          </div>
+            {/* list of styled links */}
+            <Button variant="ghost" className="justify-start" size="lg">
+                <a href="/">Home</a>
+            </Button>
+            <Button variant="ghost" className="justify-start" size="lg">
+                <a href="/dashboard">Dashboard</a>
+            </Button>
         </div>
-        <SheetFooter>
-          <SheetClose asChild>
-            <Button type="submit">Save changes</Button>
-          </SheetClose>
-        </SheetFooter>
+        
       </SheetContent>
     </Sheet>
   );
