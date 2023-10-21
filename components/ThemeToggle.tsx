0e8d21fe0762689fd8788b981c68d901291c8b06
Anthony Schneider <tonyschneider3@gmail.com>
2023-10-20 19:37:37 -0500

    component: theme toggle scales on hover

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -18,7 +18,7 @@ export default function ThemeToggle({ currentTheme }: ThemeToggleProps) {
     document.documentElement.classList.toggle("dark");
   };
   return (
-      <div onClick={toggleDarkMode} className="absolute top-0 right-0 m-4">
+      <div onClick={toggleDarkMode} className="absolute top-0 right-0 m-4 hover:scale-110 transition">
         {isDarkMode ? <LightMode size="2rem" /> : <DarkMode size="2rem" />}
       </div>
   );
