3b231885a048c50db8c85fbecb64fa716a29cf4a
Anthony Schneider <tonyschneider3@gmail.com>
2023-10-21 02:43:30 -0500

    dashboard: centered content

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <main className="flex min-h-screen flex-col items-center justify-between py-24 px-16">
+      <h1>Dashboard</h1>
+    </main>
   )
 }
