44ff87c82f40d7219536b7c68e476db0b56f19b8
Anthony Schneider <tonyschneider3@gmail.com>
2023-10-20 19:14:12 -0500

    uses themetoggle instead of its own implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,9 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ThemeToggle from "@/components/ThemeToggle";
-import {useState} from "react";
-import { ThemeProvider, useThemeContext } from "@/lib/ThemeContext";
-import {
-  MdBrightness3 as DarkMode,
-  MdBrightnessHigh as LightMode,
-} from "react-icons/md";
-
+import { cookies } from 'next/headers';
 const inter = Inter({ subsets: ["latin"] });
+import Sidebar from "@/components/Sidebar";
 
 
 export default function RootLayout({
@@ -16,10 +11,13 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const loggedIn = cookies().has('loggedIn');
+  console.log("Is logged in: ", loggedIn);
   const mode: string = "dark";
   return (
     <html lang="en" className={mode}>
       <body className={inter.className}>
+        <Sidebar />
       <ThemeToggle currentTheme={mode} />
         {children}
       </body>
