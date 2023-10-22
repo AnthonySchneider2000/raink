91212c483737480f8d3c6105730a04c93ad5c8c5
Anthony Schneider <tonyschneider3@gmail.com>
2023-10-22 17:11:08 -0500

    rain: adjusted timings

--- components/Rain/Rain.tsx
+++ components/Rain/Rain.tsx
@@ -3,7 +3,7 @@ import "./rain.css"
 function Rain() {
     return (
       <div>
-          {Array(100) // You can change the number of raindrops by adjusting this value
+          {Array(10) // You can change the number of raindrops by adjusting this value
             .fill(null)
             .map((_, index) => (
               <div key={index} className="raindrop" />
