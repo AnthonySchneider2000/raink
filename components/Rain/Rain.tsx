206a5c15fb268d08c473e2ba02da1bdeef3a941b
Anthony Schneider <tonyschneider3@gmail.com>
2023-10-22 15:37:42 -0500

    rain: wrapped rain component in a div

--- components/Rain/Rain.tsx
+++ components/Rain/Rain.tsx
@@ -2,13 +2,13 @@ import "./rain.css"
 
 function Rain() {
     return (
-      <>
+      <div>
           {Array(100) // You can change the number of raindrops by adjusting this value
             .fill(null)
             .map((_, index) => (
               <div key={index} className="raindrop" />
             ))}
-      </>
+      </div>
     );
   }
 
