7b1b0d2f47a7ddf5c283baf7aa490b0b9125c2ec
Anthony Schneider <tonyschneider3@gmail.com>
2023-10-23 22:11:16 -0500

    layout: removed user is logged in message

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ThemeToggle from "@/components/ThemeToggle";
-import { cookies } from "next/headers";
 const inter = Inter({ subsets: ["latin"] });
 import Sidebar from "@/components/Sidebar";
 import { Toaster } from "@/components/ui/toaster";
@@ -12,13 +11,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const userId = cookies().get("userId");
-  let loggedIn = false;
-  if (userId) {
-    // if the name is not "", then the user is logged in
-    if(userId.value !== "") loggedIn = true;
-  } 
-  console.log("Is logged in: ", loggedIn);
   const mode: string = "dark";
   return (
     <html lang="en" className={mode}>
