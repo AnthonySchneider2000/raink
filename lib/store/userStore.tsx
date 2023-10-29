3ff15b2babba6eb1d57c37e41880839785f996b0
Anthony Schneider <tonyschneider3@gmail.com>
2023-10-29 16:22:12 -0500

    store: save user interface instead of userId

--- lib/store/userStore.tsx
+++ lib/store/userStore.tsx
@@ -1,23 +1,16 @@
 import { create } from "zustand";
+import { User } from "../types/types";
 type State = {
-    userId: string;
-    username: string;
-    isLoggedIn: boolean;
+    user: User | null;
 };
 
 type Action = {
-    setUserId: (userId: string) => void;
-    setUsername: (username: string) => void;
-    setLoginState: (isLoggedIn: boolean) => void;
+    setUser: (user: User) => void;
 };
 
 const useUserStore = create<State & Action>((set) => ({
-    userId: "",
-    username: "",
-    isLoggedIn: false,
-    setUserId: (userId: string) => set({ userId }),
-    setUsername: (username: string) => set({ username }),
-    setLoginState: (isLoggedIn: boolean) => set({ isLoggedIn }),
+    user: null,
+    setUser: (user: User) => set({ user }),
 }));
 
 export default useUserStore;
