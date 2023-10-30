f496f34331051674e8ab154887ba37e4dd46c9dc
Anthony Schneider <tonyschneider3@gmail.com>
2023-10-30 13:10:16 -0500

    dashboard: added title

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,8 @@
+"use client"
+
 import React from "react";
 import { Skeleton } from "@/components/ui/skeleton";
-
-export default function Dashboard() {
+function SkeletonContent() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start gap-[5vh] py-24 px-16">
       <h1>Dashboard</h1>
@@ -25,8 +26,23 @@ export default function Dashboard() {
         <Skeleton className="w-full h-32" />
         <Skeleton className="w-full h-32" />
         <Skeleton className="w-full h-32" />
-
       </div>
+    </main>
+  );
+}
+
+function Title() {
+  return (
+    <h1 className="font-bold text-center 2xl:text-9xl xl:text-8xl lg:text-7xl text-6xl max-[200px]:text-2xl">
+      Dashboard
+    </h1>
+  );
+}
+
+export default function Dashboard() {
+  return (
+    <main className="flex flex-col items-center justify-start gap-[5vh] py-24 px-16">
+      <Title />
     </main>
   );
 }
