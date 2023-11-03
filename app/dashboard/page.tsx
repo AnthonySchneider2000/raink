442f26d5b789ac75e1ef491a13d6c61353bc1dba
Anthony Schneider <tonyschneider3@gmail.com>
2023-11-03 13:21:27 -0500

    components: moved SkeletonContent to own component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,35 +1,8 @@
 "use client"
 
 import React from "react";
-import { Skeleton } from "@/components/ui/skeleton";
-function SkeletonContent() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-start gap-[5vh] py-24 px-16">
-      <h1>Dashboard</h1>
-      <Skeleton className="w-full h-16" />
-      <Skeleton className="w-full h-32" />
-      <div className="grid grid-cols-2 gap-8 w-full">
-        <Skeleton className="w-full h-32" />
-        <Skeleton className="w-full h-32" />
-      </div>
+import { Button } from "@/components/ui/button";
 
-      <div className="grid grid-cols-4 gap-8 w-full">
-        <Skeleton className="w-full h-16" />
-        <Skeleton className="w-full h-16" />
-        <Skeleton className="w-full h-16" />
-        <Skeleton className="w-full h-16" />
-        <Skeleton className="w-full h-16" />
-        <Skeleton className="w-full h-16" />
-        <Skeleton className="w-full h-16" />
-        <Skeleton className="w-full h-16" />
-        <Skeleton className="w-full h-32" />
-        <Skeleton className="w-full h-32" />
-        <Skeleton className="w-full h-32" />
-        <Skeleton className="w-full h-32" />
-      </div>
-    </main>
-  );
-}
 
 function Title() {
   return (
---
+++ components/SkeletonContent.tsx
@@ -0,0 +1,29 @@
+import { Skeleton } from "@/components/ui/skeleton";
+export default function SkeletonContent() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-start gap-[5vh] py-24 px-16">
+        <h1>Dashboard</h1>
+        <Skeleton className="w-full h-16" />
+        <Skeleton className="w-full h-32" />
+        <div className="grid grid-cols-2 gap-8 w-full">
+          <Skeleton className="w-full h-32" />
+          <Skeleton className="w-full h-32" />
+        </div>
+  
+        <div className="grid grid-cols-4 gap-8 w-full">
+          <Skeleton className="w-full h-16" />
+          <Skeleton className="w-full h-16" />
+          <Skeleton className="w-full h-16" />
+          <Skeleton className="w-full h-16" />
+          <Skeleton className="w-full h-16" />
+          <Skeleton className="w-full h-16" />
+          <Skeleton className="w-full h-16" />
+          <Skeleton className="w-full h-16" />
+          <Skeleton className="w-full h-32" />
+          <Skeleton className="w-full h-32" />
+          <Skeleton className="w-full h-32" />
+          <Skeleton className="w-full h-32" />
+        </div>
+      </main>
+    );
+  }
